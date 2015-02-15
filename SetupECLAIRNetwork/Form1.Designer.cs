ec357c6bc1c8751382f0e02eb68ca62bc678aef9
adong <anthony.dong@ecl13.ec-lyon.fr>
2015-02-15 15:47:52 +0100

    Initial commit

---
+++ SetupECLAIRNetwork/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SetupECLAIRNetwork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.closeButton = new System.Windows.Forms.Button();
+            this.setButton = new System.Windows.Forms.Button();
+            this.interfaceComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            resources.ApplyResources(this.closeButton, "closeButton");
+            this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.closeButton.Name = "closeButton";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // setButton
+            // 
+            resources.ApplyResources(this.setButton, "setButton");
+            this.setButton.Name = "setButton";
+            this.setButton.UseVisualStyleBackColor = true;
+            this.setButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // interfaceComboBox
+            // 
+            resources.ApplyResources(this.interfaceComboBox, "interfaceComboBox");
+            this.interfaceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.interfaceComboBox.FormattingEnabled = true;
+            this.interfaceComboBox.Name = "interfaceComboBox";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // loginBox
+            // 
+            resources.ApplyResources(this.loginBox, "loginBox");
+            this.loginBox.Name = "loginBox";
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.loginBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.interfaceComboBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.passwordBox, 1, 1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // passwordBox
+            // 
+            resources.ApplyResources(this.passwordBox, "passwordBox");
+            this.passwordBox.Name = "passwordBox";
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Controls.Add(this.setButton);
+            this.panel1.Name = "panel1";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.setButton;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.closeButton;
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button setButton;
+        private System.Windows.Forms.ComboBox interfaceComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+
+    }
+}
+
---
+++ SetupECLAIRNetwork/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Xml.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
+
+namespace SetupECLAIRNetwork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            populateInterfaceComboBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public string GetResourceTextFile(string filename)
+        {
+            string result = string.Empty;
+            
+            //using (Stream stream = this.GetType().Assembly.
+            //           GetManifestResourceStream("SetupECLAIRNetwork.Resources." + filename))
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(GetType(), filename))
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                result = sr.ReadToEnd();
+            }
+            return result;
+        }
+
+        private void populateInterfaceComboBox()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {
+                    this.interfaceComboBox.Items.Add(nic.Name);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var interfaceName = this.interfaceComboBox.GetItemText(this.interfaceComboBox.SelectedItem);
+            var login = this.loginBox.Text;
+            var password = this.passwordBox.Text;
+
+            enableService();
+            //addCertificate();
+            enable8021X(interfaceName);
+            setCredentials(interfaceName, login, password);
+            restartInterface(interfaceName);
+            MessageBox.Show("Interface configured. You can now close this application.");
+        }
+
+        private void enableService()
+        {
+            var svc = new ServiceController("dot3svc");
+            ServiceHelper.ChangeStartMode(svc, ServiceStartMode.Automatic);
+
+            if (svc.Status != ServiceControllerStatus.Running)
+            {
+                svc.Start();
+            }
+        }
+
+        private void addCertificate()
+        {
+            byte[] bytes;
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(GetType(), "eclair.der"))
+            {
+                bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+            }
+            var cert = new X509Certificate2(bytes);
+
+            /*
+            //https://stackoverflow.com/questions/12337721/how-to-programmatically-install-a-certificate-using-c-sharp
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(cert);
+            store.Close();
+            */
+        }
+
+        private void enable8021X(string interfaceName)
+        {
+            var tempPath = Path.GetTempFileName();
+            string config_8021x = GetResourceTextFile("Wired-WinPE-PEAP-MSChapv2.xml");
+
+            using (StreamWriter sw = new StreamWriter(tempPath))
+            {
+                sw.Write(config_8021x);
+            }
+
+            Process p1 = new Process();
+            p1.StartInfo.FileName = "netsh";
+            p1.StartInfo.Arguments = "lan add profile filename=\"" + tempPath + "\" interface=\"" + interfaceName + "\"";
+            p1.StartInfo.UseShellExecute = false;
+            p1.StartInfo.CreateNoWindow = true;
+            //p1.StartInfo.RedirectStandardOutput = true;
+            p1.Start();
+            p1.WaitForExit(5000); //wait up to 5 secs
+
+            File.Delete(tempPath);
+        }
+
+        private void setCredentials(string interfaceName, string username, string password)
+        {
+            var tempPath = Path.GetTempFileName();
+            string config_eapuserinfo = GetResourceTextFile("eap_user_info.xml");
+
+            //change login and password
+            XDocument doc = XDocument.Parse(config_eapuserinfo);
+            XNamespace ns = "http://www.microsoft.com/provisioning/MsChapV2UserPropertiesV1";
+            doc.Descendants(ns + "Username").First().SetValue(username);
+            doc.Descendants(ns + "Password").First().SetValue(password);
+            doc.Descendants(ns + "LogonDomain").First().SetValue(""); //what do we put here?
+
+            doc.Save(tempPath);
+
+            Process p1 = new Process();
+            p1.StartInfo.FileName = "netsh";
+            p1.StartInfo.Arguments = "lan set eapuserdata filename=\"" + tempPath + "\" allusers=yes interface=\"" + interfaceName + "\"";
+            p1.StartInfo.UseShellExecute = false;
+            p1.StartInfo.CreateNoWindow = true;
+            //p1.StartInfo.RedirectStandardOutput = true;
+            p1.Start();
+            p1.WaitForExit(5000); //wait up to 5 secs
+
+            File.Delete(tempPath);
+        }
+
+        private void restartInterface(string interfaceName)
+        {
+            //use http://blog.opennetcf.com/2008/06/24/disableenable-network-connections-under-vista/ ?
+            Process p1 = new Process();
+            p1.StartInfo.FileName = "netsh";
+            p1.StartInfo.Arguments = "lan reconnect interface=\"" + interfaceName + "\"";
+            p1.StartInfo.UseShellExecute = false;
+            p1.StartInfo.CreateNoWindow = true;
+            //p1.StartInfo.RedirectStandardOutput = true;
+            p1.Start();
+            p1.WaitForExit(5000); //wait up to 5 secs
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+        }
+    }
+
+    public static class ServiceHelper
+    {
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern Boolean ChangeServiceConfig(
+            IntPtr hService,
+            UInt32 nServiceType,
+            UInt32 nStartType,
+            UInt32 nErrorControl,
+            String lpBinaryPathName,
+            String lpLoadOrderGroup,
+            IntPtr lpdwTagId,
+            [In] char[] lpDependencies,
+            String lpServiceStartName,
+            String lpPassword,
+            String lpDisplayName);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr OpenService(
+            IntPtr hSCManager, string lpServiceName, uint dwDesiredAccess);
+
+        [DllImport("advapi32.dll", EntryPoint = "OpenSCManagerW", ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern IntPtr OpenSCManager(
+            string machineName, string databaseName, uint dwAccess);
+
+        [DllImport("advapi32.dll", EntryPoint = "CloseServiceHandle")]
+        public static extern int CloseServiceHandle(IntPtr hSCObject);
+
+        private const uint SERVICE_NO_CHANGE = 0xFFFFFFFF;
+        private const uint SERVICE_QUERY_CONFIG = 0x00000001;
+        private const uint SERVICE_CHANGE_CONFIG = 0x00000002;
+        private const uint SC_MANAGER_ALL_ACCESS = 0x000F003F;
+
+        public static void ChangeStartMode(ServiceController svc, ServiceStartMode mode)
+        {
+            var scManagerHandle = OpenSCManager(null, null, SC_MANAGER_ALL_ACCESS);
+            if (scManagerHandle == IntPtr.Zero)
+            {
+                throw new ExternalException("Open Service Manager Error");
+            }
+
+            var serviceHandle = OpenService(
+                scManagerHandle,
+                svc.ServiceName,
+                SERVICE_QUERY_CONFIG | SERVICE_CHANGE_CONFIG);
+
+            if (serviceHandle == IntPtr.Zero)
+            {
+                throw new ExternalException("Open Service Error");
+            }
+
+            var result = ChangeServiceConfig(
+                serviceHandle,
+                SERVICE_NO_CHANGE,
+                (uint)mode,
+                SERVICE_NO_CHANGE,
+                null,
+                null,
+                IntPtr.Zero,
+                null,
+                null,
+                null,
+                null);
+
+            if (result == false)
+            {
+                int nError = Marshal.GetLastWin32Error();
+                var win32Exception = new Win32Exception(nError);
+                throw new ExternalException("Could not change service start type: "
+                    + win32Exception.Message);
+            }
+
+            CloseServiceHandle(serviceHandle);
+            CloseServiceHandle(scManagerHandle);
+        }
+    }
+}
